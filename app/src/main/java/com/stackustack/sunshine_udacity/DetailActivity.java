c12150daedc052b852f2db243246478b16fcc47d
stackustack <stackustack@gmail.com>
2015-01-28 19:21:12 +0100

    Add showing simple text details in details-view

--- app/src/main/java/com/stackustack/sunshine_udacity/DetailActivity.java
+++ app/src/main/java/com/stackustack/sunshine_udacity/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.stackustack.sunshine_udacity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -57,7 +59,10 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            String simpleForecastForSelectedDayFromMainView = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.simpleForecastFromSelectedDayTextView)).setText(simpleForecastForSelectedDayFromMainView);
             return rootView;
         }
     }
